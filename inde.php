8ac57aa0a768a8aed38fbc044c1586ec4753534f
ghislainmitahi@gmail.com <74502009+GhislainMitahi@users.noreply.github.com>
2021-03-24 16:49:54 +0100

    modification sur la page d'acceuil

--- inde.php
+++ inde.php
@@ -4,48 +4,35 @@
         <title></title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/cssStyle.css">
+        <link rel="stylesheet" href="css/avecstyle.css">
         <link rel="stylesheet" href="css/all.min.css">
     </head>
     <body>
         <header>
-          <div class="fixed-top">
-            <div class="collapse" id="navbarToggleExternalContent">
-              <div class="menu p-4">
-                <h5 class="text-warning h6"><a href="inde.php">Acceuil</a></h5>
-                <h5 class="text-warning h6"><a href="versement.php">Versement</a></h5>
-                <h5 class="text-warning h6"><a href="membres.php">Menbres & Cas social</a></h5>
-                <h5 class="text-warning h6"><a href="gestion.php">Gestion</a></h5>
-                <h5 class="text-warning h6"><a href="apropos.php">Apropos</a></h5>
-                <h5 class="text-warning h6"><a href="inscrire.php">S'inscrir</a></h5>
-              </div>
-            </div>
-            <nav class="navbar navbar-dark bg-dark">
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-            </nav>
-          </div>
+        <?php include('menu.php')?>
         </header><!--menu close -->
           <section>
-          <div class="container light shadow-sm p-3 mb-5 bg-body rounded divmain"> <!--divmain-->
-             <div class="alert alert-success shadow-lg p-3 mb-5 bg-body rounded bg-alert" role="alert">
-                <h4 class="alert-heading titre titre-alert">A la une</h4>
-                  <p class="alaune">Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-                  <hr>
-                  <p class="mb-0 alaune">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-             </div>
-             <article class="fst-normal ">
-                 <h4 class="titrearticle">AVEC</h4>
-                     <P class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sed tempora a, dicta aliquam dolorem inventore. Incidunt, voluptates! Aut ipsa voluptas rerum fugiat ab soluta tempora iure corrupti vero. Illo?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, excepturi? Itaque, neque nobis? Voluptas amet placeat dolores atque modi eveniet natus enim? Dolores magnam amet corporis voluptatem hic ipsam iste!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium delectus provident, veritatis ipsam eligendi sapiente itaque quas, illum rem earum enim, omnis architecto qui voluptates repellendus veniam officia hic ducimus?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur praesentium ab voluptas temporibus, quia corporis nostrum, tempora commodi ratione nemo sunt explicabo modi, sapiente voluptatum perferendis. Magnam culpa laboriosam quisquam.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur praesentium ab voluptas temporibus, quia corporis nostrum, tempora commodi ratione nemo sunt explicabo modi, sapiente voluptatum perferendis. Magnam culpa laboriosam quisquam.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur praesentium ab voluptas temporibus, quia corporis nostrum, tempora commodi ratione nemo sunt explicabo modi, sapiente voluptatum perferendis. Magnam culpa laboriosam quisquam.
-                     </P>
-             </article>
-             <nav class="presentation">
+            <div id="imgAvec">
+              <h1>-AVEC-</h1>
+              <P>Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Distinctio, velit.</P>
+            </div>
+            <!-- open labels pour l'evolution de l'annee  -->
+            <h4 class="text-center annee">Niveau Annenuel</h4>
+            <div class="container labele0">
+            <small class="text-muted"><?php echo'Nous sommes au mois de '.date('m').' sur 12';?></small>
+              <div class="progress"> 
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33.3%;background-color: orange">33.3%</div>
+              </div>
+            </div>
+            <div class="container labele1">
+            <small class="text-muted">12 sur 12</small>
+                <div class="progress ">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%;background-color: red">100%</div>
+                </div>
+            </div>
+            <!-- close labels -->
+            <nav class="presentation container">
                <h4 class="titrepresentation">Nos leaders</h4>
                <div class="row row-cols-1 row-cols-md-3 g-4">
                 <div class="col">
@@ -80,64 +67,61 @@
                       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to </p>
                     </div>
                     <div class="card-footer footercard">
-                      <small class="text-white">Last updated 3 mins ago</small>
+                      
                     </div>
                   </div>
                 </div>
               </div>
-             </nav>
-              <div class="card text-center Apropos">
-                <div class="card-body">
-                  <h5 class="card-title">Apropos de nous </h5>
-                  <p class="card-text text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, est ex necessitatibus, veniam neque tenetur, suscipit ducimus optio placeat blanditiis dignissimos quia illo amet laborum voluptatum quae quas obcaecati non.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore a reiciendis iusto corrupti fugiat facilis consequuntur non dignissimos aperiam! Sapiente commodi quae explicabo officiis animi in, provident quos natus sint?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis eveniet dolore iure, in ipsam eius consequatur veritatis ut placeat, quas culpa illum quisquam soluta itaque eos! Ea ex numquam architecto!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam, minus officiis ut ratione omnis nemo earum, optio ipsum voluptatibus mollitia repellendus sed. Iste numquam mollitia quae aspernatur explicabo placeat!
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, fugiat placeat. Laboriosam quidem neque maiores ducimus ipsam minus a voluptatem facere vitae, laudantium quod expedita culpa iure numquam eaque nam.
-                  </p>
+            </nav> 
+            <h1 class="text-center">Membres </h1>
+            <div>
+          <div class="row" style="margin-left:80px;margin-right:-10px">
+                      <div class="card mb-3 w-25 col-lg-" style="max-width: 540px;margin:20px 20px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img class="photo"src="images/coolman.jpg" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below </p>
+                    
+                  </div>
                 </div>
               </div>
+            </div>
 
-                <!-- grille de carte images  -->
-             </div> 
-              <a href="#" class="btn btn-color shadow-lg p-3 mb-4 bg-body rounded">En savoir plus</a> 
-          </div> 
-          <!--close divmain -->
-          <div class="container-fluid menbres"> <!--photos des Membres      https://youtu.be/0YHh9eF_I64-->
-            <h4>Nos menbres</h4>
-          <div class="row ">
-            
+            <div class="card mb-3 w-25" style="max-width: 540px;margin:20px 20px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img class="photo"src="images/coolman.jpg" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below</p>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <div class="row ">
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/coolman.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo ratione placeat officiis ipsam, quam impedit fugit ipsum saepe. Qui rerum, aperiam eveniet incidunt maiores tenetur vitae voluptatem delectus officia nemo!</p>
+            <div class="card mb-3 w-25" style="max-width: 540px;margin:20px 20px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img class="photo"src="images/coolman.jpg" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below </p>
+                    
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/coolman1.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nisi provident expedita illo ducimus id, atque libero eum. Similique soluta ullam nemo nobis reiciendis in. Odit blanditiis inventore accusamus at.</p>
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/original.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cumque pariatur delectus illum est ipsam reiciendis rerum iure? Perferendis reprehenderit dolor tempore sit doloremque cupiditate, dolorum beatae laborum eius necessitatibus.</p>
-            </div>
-          </div>
-
-          <div class="row ">
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/coolman.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo ratione placeat officiis ipsam, quam impedit fugit ipsum saepe. Qui rerum, aperiam eveniet incidunt maiores tenetur vitae voluptatem delectus officia nemo!</p>
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/coolman1.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nisi provident expedita illo ducimus id, atque libero eum. Similique soluta ullam nemo nobis reiciendis in. Odit blanditiis inventore accusamus at.</p>
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 ">
-              <img src="images/original.jpg" class="rounded-circle imgmenbres" alt="">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cumque pariatur delectus illum est ipsam reiciendis rerum iure? Perferendis reprehenderit dolor tempore sit doloremque cupiditate, dolorum beatae laborum eius necessitatibus.</p>
-            </div>
-          </div>
-          </div> <!-- close photos de membre--> 
+         </div> 
+         </div>
           <!-- commentaire et guide-->
           <div class="container commentaireguid">
             <div class="row">
