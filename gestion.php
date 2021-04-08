2ab2fee7d68ff5eb0470ad7b620939f59ec840cf
ghislainmitahi@gmail.com <74502009+GhislainMitahi@users.noreply.github.com>
2021-04-08 12:18:25 +0200

    indexAdmin

--- gestion.php
+++ gestion.php
@@ -1,131 +1,115 @@
 <!DOCTYPE html>
 <htlm>
-    <?php include("head.php")?>
-            <body>
-                <header>
-                   <?php include("menu.php")?>
-                </header>
-                
+  <?php include("head.php") ?>
+
+  <body>
+    <header>
+      <?php include("menu.php") ?>
+    </header>
 
 
 
 
-<div class="container-fluid gestion">
-<h1 class="text-center bgcolor">Gestion</h1>
-<p class="text-center gestionP">Information sur chaque membre de l'association</p>
-  <table class="table">
-  <caption>Dans cette table il n'y a que les informartions essentielles à connaitre.</caption>
-  <thead>
-    <tr>
-      <th scope="col">N.ord</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Montant déjà payé</th>
-      <th scope="col">Dettes</th>
-      <th scope="col">Delais de payement</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="colone">
-      <th scope="row">1</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">2</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">3</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">4</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">5</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">6</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">7</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">8</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-    <tr class="colone">
-      <th scope="row">9</th>
-      <td>Ghislain ghislain ghislain</td>
-      <td>50 USD</td>
-      <td>10 USD</td>
-      <td>1mois</td>
-      <td><span class="btn buttonbg">Plus</span></td>
-    </tr>
-  </tbody>
-</table>
-</div>
-
-
-             
-                
-        
-        
-        <?php include("footer.php")?>
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+    <div class="container-fluid gestion">
+      <h1 class="text-center bgcolor">Gestion</h1>
+      <p class="text-center gestionP">Information sur chaque membre de l'association</p>
+      <table class="table">
+        <caption>Dans cette table il n'y a que les informartions essentielles à connaitre.</caption>
+        <thead>
+          <tr>
+            <th scope="col">N.ord</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Montant déjà payé</th>
+            <th scope="col">Dettes</th>
+            <th scope="col">Delais de payement</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="colone">
+            <th scope="row">1</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">2</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">3</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">4</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">5</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">6</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">7</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">8</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+          <tr class="colone">
+            <th scope="row">9</th>
+            <td>Ghislain ghislain ghislain</td>
+            <td>50 USD</td>
+            <td>10 USD</td>
+            <td>1mois</td>
+            <td><span class="btn buttonbg">Plus</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
 
 
 
-<!-- <div class="card mb-3">
-              <div class="row no-gutters">
-                <div class="col-md-4">
-                  <img class="photo"src="images/coolman.jpg" alt="...">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below </p>
-                  </div>
-                </div>
-              </div>
-            </div> -->
+
+
+    <?php include("footer.php") ?>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+
+  </html>
---
+++ indexAdmin.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<htlm>
+<?php include("head.php")?>
+  <body>
+       <header> 
+        <?php include('menuAdmin.php')?>
+
+        </header><!--menu close -->
+        
+        <div class="container-fluid">
+          <section>
+            <!-- open labels pour l'evolution de l'annee  -->
+           <div id='toMenu'>
+            <h4 class="text-center annee">Niveau Annenuel</h4>
+            <div class="labele0">
+            <small class="text-muted"><?php echo'Nous sommes au mois de '.date('m').' sur 12';?></small>
+              <div class="progress"> 
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33.3%;background-color: orange">33.3%</div>
+              </div>
+            </div>
+            <div class="labele1">
+            <small class="text-muted">12 sur 12</small>
+                <div class="progress ">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%;background-color: rgb(5, 206, 92)">100%</div>
+                </div>
+            </div>
+            </div>
+
+           
+            <!-- Apropos de l'entreprises  -->
+
+            <div class="row date2">
+                    <div class="col-6 col-md-3 col-lg-3 text-center">
+                          <p class="begyear">1996</p>
+                          <label>Année de création</label>
+                      </div>
+
+                      <div class="col-6 col-md-3 col-lg-3 text-center"> 
+                        <p class="pbegyear"><?php echo "" .date('d');?></p>
+                        <label>Aujourd'hui</label>
+                      </div>
+
+                      <div class="col-6 col-md-3 col-lg-3 text-center">
+
+                        <p class="begyear"><?php echo "" .date('m');?></p>
+                        <label>Actuel mois</label>
+                      </div>
+
+                      <div class="col-6 col-md-3 col-lg-3 text-center">
+                        <p class="begyear"><?php echo "" .date('y');?></p>
+                        <label>Actuelle Année</label>
+                      </div>
+                  </div>    
+                  <!-- debut tableau    dette  -->
+
+                   <div class="container-fluid gestion">
+      <h1 class="text-center bgcolor">Etat actuel </h1>
+      <p class="text-center gestionP">Information sur chaque membre de l'association</p>
+       <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Cas sociaux visités</th>
+            <th scope="col"> Sortie USD</th>           
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="colone">
+            <th scope="row">10</th>
+            <td>110</td>           
+            <td><span class="btn buttonbg">Voir plus</span></td>
+          </tr>
+         
+        </tbody>
+      </table>
+      <!-- Cas Social -->
+
+       <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Cas sociaux visités</th>
+            <th scope="col"> Sortie USD</th>           
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="colone">
+            <th scope="row">10</th>
+            <td>110</td>           
+            <td><span class="btn buttonbg">Voir plus</span></td>
+          </tr>
+         
+        </tbody>
+      </table>
+    
+       <!-- Cas Social -->
+
+<h6 style="color:blue" >Tresor disponible</h6>
+     <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">montat USD</th>
+            <th scope="col">  Montat FC</th>           
+            <th scope="col"> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="colone">
+            <th scope="row">6000</th>
+            <td>500.000</td>           
+            <td><span class="btn buttonbg">Voir plus</span></td>
+          </tr>
+         
+        </tbody>
+      </table>
+
+       <!-- <table class="table">
+        <thead>
+          <tr>           
+            <th scope="col"> </th>
+            <th scope="col"></th>  
+            <th scope="col"> </th>   
+
+                  
+
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="colone">
+            <th scope="row">10</th>
+            <td>110</td>           
+            <td><span class="btn buttonbg">Voir plus</span></td>
+          </tr>
+         
+        </tbody>
+      </table> -->
+    </div>
+    </div>
+
+
+
+<div class="text-center" style="height:40px;background:red; "> <a href="#toMenu">Aller vers l'ancre</a></div>
+  
+            
+                
+                <script src="js/jquery.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+            </body>
+        </htlm>
